ab2beb92f46b619f979772ca4013267417a59887
anonyindian <anony@ferrisgram.org>
2022-07-24 00:18:49 +0530

    add license header in main.py

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
+# Mood Calculator - A part of pyprac
+# Copyright (C) 2022 
+# Veer Pratap Singh <hello@veer.codes>
+# => github.com/anonyindian
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser Public License for more details.
+#
+# You should have received a copy of the GNU Lesser Public License
+# along with this program. If not, see [http://www.gnu.org/licenses/].
+
+# ====================================================================
+
 import os
 
 scripts = []
@@ -46,3 +66,8 @@ script = finder().replace(" ", "_").lower()
 import os
 os.system("clear")
 os.system(f"python {script}.py")
+
+# ====================================================================
+# Thank You for checking it out!
+# Regards, Veer
+# ====================================================================
